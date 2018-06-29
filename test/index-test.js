d50797fe43f695c2ddeb5f7bd6e6726d67dc7993
Molly Oretsky <mollyoretsky@gmail.com>
2018-06-29 23:00:10 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,6 @@
-function shout(string) {return string.toUpperCase()
-}
+function shout(string) {
+  return string.toUpperCase()
+} 
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
